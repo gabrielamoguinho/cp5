ca8791a97c6c2f0ac40ed0779de56e60a54344cf
gabrielamoguinho <gmgoncalves26@gmail.com>
2024-10-15 22:45:58 -0300

    corringindo mais alguns detalhes

--- src/app/busca/page.tsx
+++ src/app/busca/page.tsx
@@ -5,11 +5,11 @@ import { Header } from "@/components/Header/Header";
 import { Input } from "@/components/Input/Input";
 import { Button } from "@/components/Button/Button";
 import UserContext from "@/context/UserContext";
-import { LoadLoginState } from "@/helpers/Loadloginstate";
+import { Loadloginstate } from "@/helpers/Loadloginstate";
 
 
 export default function Busca() {
-    LoadLoginState();
+    Loadloginstate();
     const router = useRouter();
     const [cityName, setCityName] = useState<string>("");
     const [cityList, setCityList] = useState([]);
